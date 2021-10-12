0b3b08c62af5feceb0355d358aacbda255d87f88
viclafouch <victor.dlf@outlook.fr>
2021-10-12 17:24:26 +0200

    chore: install webpack & lint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        printWidth: 80,
+        tabWidth: 2,
+        jsxSingleQuote: false,
+        trailingComma: 'none',
+        arrowParens: 'avoid'
+      }
+    ]
+  }
+}
---
+++ webpack.config.ts
@@ -0,0 +1,76 @@
+import webpack from 'webpack'
+import CopyPlugin from 'copy-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import browsers from './browsers'
+import * as path from 'path'
+import open from 'open'
+
+const currentBrowser = process.env.TARGET
+
+if (!currentBrowser || !browsers.includes(currentBrowser)) {
+  throw new Error(
+    `Please specify the TARGET environment. \n Possible values: ${browsers}`
+  )
+} else {
+  console.info(
+    `\x1b[1;32mLinkedIn-Formatter@${process.env.npm_package_version}\x1b[m`
+  )
+  console.info(`\x1b[1;32mBuilding for ${currentBrowser}...\x1b[m`)
+}
+
+const outputPath = path.join(__dirname, 'build', 'chrome')
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const createConfig = (env: any, argv: any): webpack.Configuration => {
+  const IS_DEV = argv.mode === 'development'
+  const config: webpack.Configuration = {
+    watch: IS_DEV,
+    devtool: false,
+    entry: './src/main.ts',
+    output: {
+      filename: '[name].bundle.js',
+      path: outputPath
+    },
+    plugins: [
+      {
+        apply: compiler => {
+          compiler.hooks.afterDone.tap('open', () => {
+            if (IS_DEV) {
+              open('https://www.linkedin.com')
+            }
+          })
+        }
+      },
+      new webpack.ProgressPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, 'manifest.json'),
+            to: outputPath,
+            force: true,
+            transform: function (content) {
+              const manifestContent = JSON.parse(content.toString())
+              return Buffer.from(
+                JSON.stringify(
+                  {
+                    description: process.env.npm_package_description,
+                    version: process.env.npm_package_version,
+                    ...manifestContent
+                  },
+                  null,
+                  4
+                )
+              )
+            }
+          }
+        ]
+      })
+    ]
+  }
+  if (!IS_DEV && config.plugins) {
+    config.plugins.push(new CleanWebpackPlugin())
+  }
+  return config
+}
+
+export default createConfig
